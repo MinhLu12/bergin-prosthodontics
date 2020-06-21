db9e27428d77a52422cfea4637cdf9339dcb3d19
Summer Mickelsen <micksumm@isu.edu>
2020-06-21 15:48:36 -0700

    Styled the before and after component.

--- src/components/BeforeAndAfter/BeforeAndAfter.js
+++ src/components/BeforeAndAfter/BeforeAndAfter.js
@@ -2,19 +2,25 @@ import React from 'react';
 import cn from './BeforeAndAfter.module.scss';
 
 const BeforeAndAfter = () => {
-    return (
-        <>
-            <div className ={cn.BeforeAndAfter}>
-                <img src="/FrontPageBeforeAndAfter/Before.png"></img>
-                <img src="/FrontPageBeforeAndAfter/After.png"></img>
-                <div>
-                    <p> Creating Smiles with a Gentle touch.</p>
-                    <a className={cn.ClickHere} href = "#" > Click here </a> 
-                    <p className={cn.TextInliner}> to view our full smile gallery.</p>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={cn.BeforeAndAfter}>
+        <div className={cn.Image}>
+          <img src="/FrontPageBeforeAndAfter/Before.png"></img>
+          <img src="/FrontPageBeforeAndAfter/After.png"></img>
+        </div>
+        <div className={cn.Text}>
+          <span className={cn.Header}> Creating Smiles with a Gentle touch.</span>
+
+          <div>
+            <a className={cn.ClickHere} href="#">Click here</a>
+            <span className={cn.TextInliner}>&nbsp; to view our full smile gallery.</span>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default BeforeAndAfter;
