7d0d276eccd5429481cedb9f970969886a59e5f7
Summer Mickelsen <micksumm@isu.edu>
2020-06-21 19:47:47 -0700

    Addressed Feedback, including style changes and font changes.

--- src/components/BeforeAndAfter/BeforeAndAfter.js
+++ src/components/BeforeAndAfter/BeforeAndAfter.js
@@ -7,19 +7,19 @@ const BeforeAndAfter = () => {
   return (
     <>
       <div className={cn.BeforeAndAfter}>
-        <div className={cn.Image}>
+
+        <div className={cn.Images}>
           <img src={teethBefore}></img>
           <img src={teethAfter}></img>
         </div>
+
         <div className={cn.Text}>
-          <span className={cn.Header}> Creating Smiles with a Gentle touch.</span>
-
-          <div>
-            <a className={cn.ClickHere} href="#">Click here</a>
-            <span className={cn.TextInliner}>&nbsp; to view our full smile gallery.</span>
-          </div>
-
+          <span className={cn.Header}>Creating Smiles with a Gentle touch.</span>
+          <span className={cn.Captions}>
+            <a href="#">Click here</a> to view our full smile gallery.
+          </span>
         </div>
+        
       </div>
     </>
   )
