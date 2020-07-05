2f192ecae50e81598e9c4413ef07f5e9c900e3fd
Summer Mickelsen <micksumm@isu.edu>
2020-07-04 21:06:28 -0700

    Updated the font in the Nav and Contact. Changed spacing in the Summary.

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -8,22 +8,16 @@ const Summary = () => {
 		<>
 			<div className={cn.Summary}>
 
-				<div className={cn.Welcome_Text}>
-
-					<div className={cn.Left_Space}></div>
-					<div className={cn.Right_Box}>
-						<div className={cn.Title_Text}>
-							<span>Welcome to</span>
-							<span>Bergin Prosthodontics</span>
-						</div>
-						<hr className={cn.Break}></hr>
-							<div className={cn.Paragraph}>
-							<p>lots of text here about the office lots of text here about the office</p>
-							</div>
-						
-					</div>
-
-				</div>
+                <div className={cn.Welcome_Text}>
+                    <div className={cn.Title_Text}>
+                        <span>Welcome to</span>
+                        <span>Bergin Prosthodontics</span>
+                    </div>
+                    <hr className={cn.Break}></hr>
+                    <div className={cn.Paragraph}>
+                        <p>lots of text here about the office lots of text here about the office</p>
+                    </div>
+                </div>
 
 				<div className={cn.Bergin_Image}>
 					<img src={DrBergin}></img>
