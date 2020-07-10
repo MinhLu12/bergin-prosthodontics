c8848bcdb48e45398c3a255a85828865c4140ffc
Summer Mickelsen <micksumm@isu.edu>
2020-07-09 19:12:49 -0700

    Fix app and title

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import {
 } from "react-router-dom";
 import Home from "pages/home/Home";
 import Navigation from "./components/Navigation/Navigation";
-import Summary from "./components/Summary/Summary";
-import ContactInfo from "./components/ContactInfo/ContactInfo";
-import BeforeAndAfter from "./components/BeforeAndAfter/BeforeAndAfter";
-import FeaturedServices from "./components/FeaturedServices/FeaturedServices"
-import Title from "./components/Title/Title";
 
 export default function App() {
 	return (
