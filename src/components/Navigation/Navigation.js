1719ec163ecd16e06f401331dc09010134036543
Minh Lu <minhlu3@gmail.com>
2020-07-14 19:30:59 -0700

    Change .vscode preferences to space correctly on save

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,28 +1,28 @@
 import React from "react";
 import TabDropdown from "./TabDropdown/TabDropdown";
-import cn from  "./Navigation.module.scss";
+import cn from "./Navigation.module.scss";
 import { AppBar, Toolbar, Button } from "@material-ui/core";
 import PhoneIcon from "@material-ui/icons/Phone";
 
 const Navigation = () => {
     return (
         <AppBar position="static" className={cn.AppBar}>
-
             <Toolbar className={cn.Toolbar}>
-                <p className={cn.Name}> Bergin Prosthodontics </p>
+                <p className={cn.Name}> Bergin Prosthodontics</p>
                 <div className={cn.Contact}>
                     <PhoneIcon className={cn.Phone_Icon} fontSize="small" />
                     <span className={cn.Phone_Number}>206-866-6466</span>
-                    <Button variant="contained" className={cn.Button}>Contact Us</Button>
+                    <Button variant="contained" className={cn.Button}>
+                        Contact Us
+                    </Button>
                 </div>
             </Toolbar>
 
             <div className={cn.Tab_Dropdown}>
                 <TabDropdown></TabDropdown>
             </div>
-
         </AppBar>
-    )
-}
+    );
+};
 
 export default Navigation;
