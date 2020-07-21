d22b412885b77f420c4531ef24be919f9c095c4f
Summer Mickelsen <micksumm@isu.edu>
2020-07-21 12:38:35 -0700

    Added the Meetdrbergin page.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "pages/home/Home";
+import Meetdrbergin from "pages/meetdrbergin/Meetdrbergin";
 import Navigation from "./components/Navigation/Navigation";
 
 export default function App() {
-    return (
-        <Router>
-            <Navigation></Navigation>
+	return (
+		<Router>
+			<Navigation></Navigation>
 
-            <Switch>
-                <Route path="/contact">Contact Page</Route>
-
-                <Route path="/">
-                    <Home></Home>
-                </Route>
-            </Switch>
-        </Router>
-    );
+			<Switch>
+				<Route path="/contact">Contact Page</Route>
+				<Route path="/Meetdrbergin" component={Meetdrbergin} />
+				<Route path="/">
+					<Home></Home>
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
---
+++ src/pages/meetdrbergin/Meetdrbergin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import cn from "./Meetdrbergin.module.scss";
+
+export default function Meetdrbergin() {
+    return (
+        <>
+            <p> Meet Dr. Bergin</p>
+        </>
+    );
+}
